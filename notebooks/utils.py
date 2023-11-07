ab8c42a7bbb4ec5d705e732844667c10a8541b20
avishreekh <avishreekh@gmail.com>
2023-11-07 13:23:10 -0500

    Add custom methods in notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -11,7 +11,16 @@ def qed_compute_metrics(eval_preds):
         preds = eval_preds.predictions > 0
     avg_ones = np.mean(preds)
     acc = np.mean(preds == eval_preds.label_ids)
-    return {"Accuracy" : acc, "Avg Ones" : avg_ones}
+    try:
+        # In case the eval metrics also include losses, just return the mean loss
+        loss = np.mean(eval_preds.losses)
+        return {
+            "Accuracy": acc,
+            "Avg Ones": avg_ones,
+            "Loss": loss
+        }
+    except:
+        return {"Accuracy" : acc, "Avg Ones" : avg_ones}
 
 def succ_compute_metrics(eval_preds):
     if isinstance(eval_preds.predictions, tuple):
