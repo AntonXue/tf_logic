99b78d1f83a64792ad96a3356977252f52afe2bb
Anton Xue <antonxue@seas.upenn.edu>
2024-01-29 16:26:31 -0500

    Add learned attacks support

--- experiments/learned_attacks.py
+++ experiments/learned_attacks.py
@@ -9,7 +9,7 @@ from my_datasets.attack_datasets import AutoregKStepsTokensAttackDataset
 from utils.model_loader_utils import load_next_state_model_from_wandb
 from models.attack_models import ForceOutputWithAppendedAttackTokensWrapper
 from transformers import TrainingArguments, Trainer
-from utils.metrics_utils import autoreg_ksteps_metrics
+from utils.metrics import autoreg_ksteps_metrics
 
 DEFAULT_DUMP_DIR = str(Path(DUMP_DIR, "learned_attack_experiments"))
 
@@ -32,21 +32,20 @@ def synexp_args_to_wandb_run_name(param_instance):
             + f"_atk-{param_instance['base_attack_model_name']}_nat{param_instance['num_attack_tokens']}"
         )
 
-def run_attack(attacker_model, reasoner_model, dataset, param_instance, output_dir=DEFAULT_DUMP_DIR):
+def run_attack(attacker_model, reasoner_model, train_dataset, eval_dataset, param_instance, output_dir=DEFAULT_DUMP_DIR):
     """Train the attacker model on the given dataset."""
     training_args = TrainingArguments(
         str(Path(output_dir, synexp_args_to_wandb_run_name(param_instance))),
-        num_train_epochs = param_instance["num_epochs"],
-        per_device_train_batch_size = param_instance["train_batch_size"],
-        per_device_eval_batch_size = param_instance["eval_batch_size"],
+        num_train_epochs = param_instance["num_attack_epochs"],
+        per_device_train_batch_size = param_instance["attack_train_batch_size"],
+        per_device_eval_batch_size = param_instance["attack_eval_batch_size"],
         auto_find_batch_size = True,
         evaluation_strategy = "epoch",
         report_to = "wandb",
-        run_name = synexp_args_to_wandb_run_name(args),
+        run_name = synexp_args_to_wandb_run_name(param_instance),
         logging_steps = 10,
         warmup_ratio = 0.10,
-        save_strategy = "no",
-        load_best_model_at_end = True)
+        save_strategy = "no")
 
     trainer = Trainer(
         attacker_model,
@@ -81,6 +80,7 @@ if __name__ == "__main__":
     print("Running with config: ", config)
 
     for param_dict in config:
+        print("Param dict:", param_dict)
         for param_tuple in itertools.product(*param_dict.values()):
             param_instance = {}
             for i, param in enumerate(param_tuple):
@@ -115,11 +115,11 @@ if __name__ == "__main__":
                     param_instance["max_chain_len"],
                 ),
                 num_steps=param_instance["num_steps"],
-                dataset_len=param_instance["train_len"] + param_instance["eval_len"],
+                dataset_len=param_instance["attack_train_len"] + param_instance["attack_eval_len"],
             )
 
             train_dataset, eval_dataset = torch.utils.data.random_split(
-                big_dataset, [param_instance["train_len"], param_instance["eval_len"]]
+                big_dataset, [param_instance["attack_train_len"], param_instance["attack_eval_len"]]
             )
 
             reasoner_model = load_next_state_model_from_wandb(
