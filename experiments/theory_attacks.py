abd5d5ea4cb60d43861d17a6f14e3d4c163bc7c4
Anton Xue <antonxue@seas.upenn.edu>
2024-04-09 18:50:33 -0400

    repeat tokens attack

--- experiments/theory_attacks.py
+++ experiments/theory_attacks.py
@@ -83,15 +83,16 @@ def run_big_token_attack(args):
 
                 for batch in pbar:
                     batch_tokens = batch["tokens"].to(args.device)
-                    s_tgt = (torch.rand(args.batch_size, n) < 0.5).long().to(args.device)
-                    kappa_token = s_tgt - kappa * (1 - s_tgt)
-                    adv_token = torch.cat([torch.zeros_like(s_tgt), kappa_token.float()], dim=1)
+                    tgt_state = (torch.rand(args.batch_size, n) < 0.5).long().to(args.device)
+                    tgt_scaled = tgt_state - kappa * (1 - tgt_state)
+
+                    adv_token = torch.cat([torch.zeros_like(tgt_scaled), tgt_scaled.float()], dim=1)
                     all_tokens = torch.cat([batch_tokens, adv_token.view(-1,1,2*n)], dim=1)
                     out = model(all_tokens)
                     pred = (out.logits[:,0] > 0).long()
                     num_dones += batch_tokens.size(0)
-                    acc_elem_hits += (s_tgt == pred).float().mean(dim=1).sum()
-                    acc_state_hits += ((s_tgt == pred).sum(dim=1) == n).sum()
+                    acc_elem_hits += (tgt_state == pred).float().mean(dim=1).sum()
+                    acc_state_hits += ((tgt_state == pred).sum(dim=1) == n).sum()
                     elems_acc = acc_elem_hits / num_dones
                     states_acc = acc_state_hits / num_dones
 
@@ -116,15 +117,70 @@ def run_big_token_attack(args):
             print(f"Done with: trseed {train_seed}, n {n}, d {d}")
 
 
+def run_repeat_token_attack(args):
+    config = Namespace(**json.load(open(args.config_file)))
+    assert config.num_samples % args.batch_size == 0
 
-def run_repeat_tokens_attack(model, dataset, num_repeats, num_samples):
-    pass
-    return {
-        "num_samples": num_samples,
-        "state_acc": None,
-        "elems_acc": None,
-    }
+    saveto_file = Path(args.output_dir, "theory_attack_repeat_token.csv")
+    print(f"Will save to: {saveto_file}")
 
+    df = pd.DataFrame(columns=[
+        "train_seed", "num_vars", "embed_dim", "num_repeats", "elems_acc", "states_acc"
+    ])
+
+    df_idx = 0
+
+    for train_seed in config.train_seeds:
+        for nd_pair in config.nd_pairs:
+            n, d = nd_pair
+            model, dataset = load_model_and_dataset_from_big_grid(
+                embed_dim = d,
+                num_vars = n,
+                num_steps = 3, # TODO: change
+                seed = train_seed,
+                dataset_len = config.num_samples,
+            )
+
+            model.eval().to(args.device)
+            dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+
+            for num_repeats in config.num_repeats:
+                num_dones, acc_elem_hits, acc_state_hits = 0, 0, 0
+                pbar = tqdm(dataloader)
+
+                for batch in pbar:
+                    batch_tokens = batch["tokens"].to(args.device)
+                    tgt_state = (torch.rand(args.batch_size, n) < 0.5).long().to(args.device)
+                    tgt_repeated = tgt_state.view(-1,1,n).repeat(1,num_repeats,1)
+                    adv_tokens = torch.cat([torch.zeros_like(tgt_repeated), tgt_repeated], dim=2)
+                    all_tokens = torch.cat([batch_tokens, adv_tokens.float()], dim=1)
+                    out = model(all_tokens)
+                    pred = (out.logits[:,0] > 0).long()
+                    num_dones += batch_tokens.size(0)
+                    acc_elem_hits += (tgt_state == pred).float().mean(dim=1).sum()
+                    acc_state_hits += ((tgt_state == pred).sum(dim=1) == n).sum()
+                    elems_acc = acc_elem_hits / num_dones
+                    states_acc = acc_state_hits / num_dones
+
+                    desc_str = f"n {n}, d {d}, nr {num_repeats}: "
+                    desc_str += f"N {num_dones}, elems {elems_acc:.3f}, states {states_acc:.3f}"
+                    pbar.set_description(desc_str)
+
+                this_df = pd.DataFrame({
+                    "train_seed": train_seed,
+                    "num_vars": n,
+                    "embed_dim": d,
+                    "num_repeats": num_repeats,
+                    "elems_acc": elems_acc.item(),
+                    "states_acc": states_acc.item(),
+                }, index=[df_idx])
+
+                df_idx += 1
+
+                df = pd.concat([df, this_df])
+                df.to_csv(saveto_file)
+
+            print(f"Done with: trseed {train_seed}, n {n}, d {d}")
 
 
 if __name__ == "__main__":
@@ -135,7 +191,7 @@ if __name__ == "__main__":
     if args.attack_name == "big_token_attack":
         run_big_token_attack(args)
     elif args.attack_name == "repeat_token_attack":
-        pass
+        run_repeat_token_attack(args)
 
 
 
